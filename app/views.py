371670e53bee1b20a728a70b3fd207e3ddfab429
thanhhuynhk17 <csk17fit@gmail.com>
2022-04-06 09:54:09 +0700

    get client ip

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from flask import request
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template('public/index.html', ip=request.remote_addr)
+    return render_template('public/index.html', ip=request.environ['REMOTE_ADDR'])
 
 @app.route("/card/<name>", methods=['GET'])
 def profile(name=None):
